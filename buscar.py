2e7b9f067ec9888e2b4cf3e20b21ffbddc6c10e8
Gustavo dos Santos Garcia <gustavogarcia1204@gmail.com>
2025-10-29 20:28:41 -0300

    Update buscar.py

--- buscar.py
+++ buscar.py
@@ -22,13 +22,12 @@ def carregar_dados_e_preparar_busca():
         with open(ARQUIVO_DADOS, 'r', encoding='utf-8') as f:
             documentos = json.load(f)
 
-        # 1. Pega o texto e o nome de cada documento
-        textos = [doc['texto_completo'] for doc in documentos]
 
-        # 2. "Treina" o vetorizador com nossos textos
+        textos = [doc['texto_completo'] for doc in documentos]
+        
         print("Preparando motor de busca (TF-IDF)...")
         vectorizer = TfidfVectorizer(
-            stop_words=None, # Você pode adicionar stop words em português se quiser
+            stop_words=None, 
             ngram_range=(1, 2) # Considera palavras sozinhas e pares (ex: "inteligencia", "artificial", "inteligencia artificial")
         )
         matriz_tfidf = vectorizer.fit_transform(textos)
@@ -50,23 +49,23 @@ def encontrar_documentos_relevantes(query_usuario, top_n=3):
         print("Motor de busca não iniciado. Carregando dados...")
         carregar_dados_e_preparar_busca()
 
-    # 1. Transforma a busca do usuário usando o MESMO vetorizador
+
     query_tfidf = vectorizer.transform([query_usuario])
 
-    # 2. Calcula a "pontuação" de similaridade
+
     similaridades = cosine_similarity(query_tfidf, matriz_tfidf).flatten()
 
-    # 3. Pega os índices dos N mais relevantes (em ordem)
+
     indices_relevantes = similaridades.argsort()[:-top_n-1:-1]
 
-    # 4. Monta a lista de resultados
+
     resultados = []
     for i in indices_relevantes:
         if similaridades[i] > 0: # Só retorna se tiver alguma relevância
             resultado = {
                 "nome_arquivo": documentos[i]['nome_arquivo'],
                 "tipo_documento": documentos[i]['tipo_documento'],
-                "texto_contexto": documentos[i]['texto_completo'], # Retorna o texto todo
+                "texto_contexto": documentos[i]['texto_completo'],
                 "pontuacao": similaridades[i]
             }
             resultados.append(resultado)
@@ -74,7 +73,7 @@ def encontrar_documentos_relevantes(query_usuario, top_n=3):
     return resultados
 
 # --- Teste Rápido ---
-# Isso só roda se você executar 'python buscar.py'
+
 if __name__ == "__main__":
     print("--- Teste do Motor de Busca (Objetivo 3) ---")
     carregar_dados_e_preparar_busca()
